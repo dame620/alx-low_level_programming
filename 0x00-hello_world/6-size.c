58927079a9f45c90192ce74b510b0674b392b5a6
dame620 <damendiaye620@gmail.com>
2022-09-11 15:25:10 -0700

    correct-print-f

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,11 @@
  */
 int main(void)
 {
-	printf("size of char %d byte(s)\n size of int %d byte(s)\n size of long int %d byte(s)\n size of long long int %d byte(s)\n size of float %d byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+	printf("size of char: %d byte(s)\n",  sizeof(char));
+	printf("size of int: %d byte(s)\n",  sizeof(int));
+	printf("size of long: int %d byte(s)\n",  sizeof(long int));
+	printf("size of long: long int %d byte(s)\n",  sizeof(long long int));
+	printf("size of float: %d byte(s)\n",  sizeof(float));
+
 	return (0);
 }
