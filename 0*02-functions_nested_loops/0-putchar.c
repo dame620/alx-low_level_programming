000425a2e7048658ec484be5df8d0c0792e0a46c
dame620 <damendiaye620@gmail.com>
2022-09-18 12:21:45 -0700

    first-commit

---
+++ 0*02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+ *
+ */
+int main(void)
+{
+	_putchar(char _putchar);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+the first line of my readme file
+write a programme that print positve negative or zero
