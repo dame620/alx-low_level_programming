521701d458447f8db530b4a12860155a49cc640e
dame620 <damendiaye620@gmail.com>
2022-09-18 18:04:38 -0700

    print-lower-char

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	char lower_char;
+
+	for(lower_char = 'a'; lower_char <= 'z'; lower_char++)
+	{
+		putchar(lower_char);
+	}
+	putchar('\n');
+	return (0);
+}
